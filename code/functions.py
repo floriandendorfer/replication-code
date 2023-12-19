00eb02f0849c31d1f5b0de8259d1be8dade6b4a1
Florian Dendorfer <95039693+floriandendorfer@users.noreply.github.com>
2023-12-19 14:41:34 -0500

    update 2

--- code/functions.py
+++ code/functions.py
@@ -6,7 +6,7 @@ Created on Thu Mar 16 09:20:42 2023
 """
 
 import numpy as np
-from scipy.special import expit
+from scipy.sparse import csr_matrix
 
 #################################  Functions  #################################
 
@@ -18,19 +18,252 @@ def cartesian_product(*arrays):
         arr[...,i] = a
     return arr.reshape(-1, la)
 
-def U(p,theta,t,S,f):
-    a,b,alpha,beta,gamma=theta
-    k = S[:,0:1,:]
-    n = S[:,1:2,:]
-    return gamma*( (a+k)/(a+b+n) ) + np.repeat(np.array([beta])[np.newaxis,:, :],len(S),axis=0) + alpha*(p*f-t)
+def U_s(p,theta,t,params):
+    a,b,alpha,beta,gamma,sigma=theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    return gamma*( (a+S[:,0:1])/(a+b+S[:,1:2]) ) + S[:,2:] @ beta + alpha*(p*(1+f) - t)
 
-def q(p,P,s,theta,t,S,f,mu):
+def ccp_jg_s(p,P,s,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    U_j = np.array([np.diagonal(U_s(p,theta,t,S,f),axis1=0,axis2=1)]).T
+    U_all = np.array([np.diagonal(U_s(p,theta,t,S,f),axis1=0,axis2=1)]).T
+    
+    return np.exp(U_j/(1-sigma))/( np.exp(U_j/(1-sigma)) - np.tile(np.exp(U_all/(1-sigma)),(1,U_j.shape[1]))
+                                  + S[:,2:] @  np.array([[ float(s[:,:231] @ np.exp(U_all/(1-sigma))[:231,:]),
+                                                          float(s[:,231:462] @ np.exp(U_all/(1-sigma))[231:462,:]),
+                                                          float(s[:,462:693] @ np.exp(U_all/(1-sigma))[462:693,:]),
+                                                          float(s[:,693:] @ np.exp(U_all/(1-sigma))[693:,:]) 
+                                                          ]]).T)
+
+def ccp_g_s(p,P,s,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    U_j = np.array([np.diagonal(U_s(p,theta,t,params),axis1=0,axis2=1)]).T
+    U_all = np.array([np.diagonal(U_s(P,theta,t,params),axis1=0,axis2=1)]).T
+    
+    D = ( np.exp(U_j/(1-sigma)) - np.tile(np.exp(U_all/(1-sigma)),(1,U_j.shape[1]))
+                                  + S[:,2:] @  np.array([[ float(s[:,:231] @ np.exp(U_all/(1-sigma))[:231,:]),
+                                                          float(s[:,231:462] @ np.exp(U_all/(1-sigma))[231:462,:]),
+                                                          float(s[:,462:693] @ np.exp(U_all/(1-sigma))[462:693,:]),
+                                                          float(s[:,693:] @ np.exp(U_all/(1-sigma))[693:,:]) 
+                                                          ]]).T)
+    return D**(1-sigma)/( 1 + ((D[[0,231,462,693],0])**(1-sigma)).sum() )
+
+def ccp_s(p,P,s,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    return ccp_jg_s(p,P,s,theta,t,params)*ccp_g_s(p,P,s,theta,t,params) 
+
+def q_s(p,P,s,theta,t,S,f,mu):
     a,b,alpha,beta,gamma = theta
-    U_j = U(p,theta,t,S,f)
-    #U_all = np.diagonal(U(P,theta,t,S,f),axis1=0,axis2=1).reshape((len(S)*4,1))
-    U_all = U(P,theta,t,S,f).reshape((len(S)*4,1))
+    U_j = U_s(p,theta,t,S,f)
+    U_all = U_s(P,theta,t,S,f).reshape((len(S)*4,1))
     q = 1 - np.exp(-mu*(np.exp(U_j)/(1 + s.reshape((1,len(S)*4)) @ np.exp(U_all) )))
     return q
 
-def pi(P,s,theta,t,S,f,mu,prices):
-    return q(np.repeat(np.repeat(prices,len(S),axis=0)[:,:,np.newaxis],4,axis=2),P,s,theta,t,S,f,mu)*np.repeat(np.repeat(prices,len(S),axis=0)[:,:,np.newaxis],4,axis=2)
+def dccp_s(p,P,s,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    ccp = ccp_s(p,P,s,theta,t,params)
+    ccp_jg = ccp_jg_s(p,P,s,theta,t,params)
+    return ccp*(1-sigma*ccp_jg - (1-sigma)*ccp)*alpha*(1+f)/(1-sigma)
+
+def d2ccp_s(p,P,s,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    ccp = ccp_s(p,P,s,theta,t,params)
+    ccp_jg = ccp_jg_s(p,P,s,theta,t,params)
+    return ((1/(1-sigma)-(sigma/(1-sigma))*ccp_jg - 2*ccp)*(1/(1-sigma)-(sigma/(1-sigma))*ccp_jg - ccp)*ccp 
+            - (sigma/((1-sigma)**2))*ccp*ccp_jg*(1-ccp_jg)
+            )*alpha*(1+f)*alpha*(1+f)
+
+def dq_s(p,P,s,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    return mu*np.exp(-mu*ccp_s(p,P,s,theta,t,params))*dccp_s(p,P,s,theta,t,params)
+
+def d2q_s(p,P,s,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    ccp = ccp_s(p,P,s,theta,t,params)
+    dccp = dccp_s(p,P,s,theta,t,params)
+    d2ccp = d2ccp_s(p,P,s,theta,t,params)
+    return mu*np.exp(-mu*ccp)*( d2ccp - mu*dccp**2 )
+
+def V_s(p,P,s,V,theta,phi_bar,t,Sub,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    q =q_s(p,P,s,theta,t,params)
+    T = T_s(p,P,s,q,theta,t,params)
+    eV = T @ V
+    V = 30*q*p.T + Sub + delta*eV + np.exp(-delta*eV/phi_bar)*phi_bar
+    return V
+    
+def dV_s(p,P,s,V,theta,phi_bar,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    q = q_s(p,P,s,theta,t,params)
+    dq = dq_s(p,P,s,theta,t,params)
+    dT = dT_s(p,P,s,dq,theta,t,params)
+    T = T_s(p,P,s,q,theta,t,params)
+    deV = dT @ V
+    dV = 30*(q + dq*p.T) + delta*deV - np.exp(-delta*(T @ V)/phi_bar)*delta*deV
+    return dV.T
+
+def d2V_s(p,P,s,V,theta,phi_bar,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    q = q_s(p,P,s,theta,t,params)
+    dq = dq_s(p,P,s,theta,t,params)
+    d2q = d2q_s(p,P,s,theta,t,params)
+    T = T_s(p,P,s,q,theta,t,params)
+    dT = dT_s(p,P,s,dq,theta,t,params)
+    d2T = d2T_s(p,P,s,d2q,theta,t,params)
+    deV = dT @ V
+    d2eV = d2T @ V
+    d2V = 30*(2*dq + d2q*p.T) + delta*d2eV - np.exp(-delta*(T @ V)/phi_bar)*delta*d2eV + np.exp(-delta*(T @ V)/phi_bar)*delta*delta*deV*deV/phi_bar
+    return d2V.T
+
+def T_s(p,P,s,q,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    uq =  (q*upsilon_r).flatten()
+    Ns = S[:,1]
+    row_terminal = np.arange(len(Ns))[Ns==nmax]
+    row = np.arange(len(Ns))[Ns<nmax]
+    column_terminal = np.arange(len(Ns))[Ns==nmax]
+    column_no = np.arange(len(Ns))[Ns<nmax]
+    column_bad = np.arange(len(Ns))[Ns<nmax]+Ns[Ns<nmax]+1
+    column_good = np.arange(len(Ns))[Ns<nmax]+Ns[Ns<nmax]+2
+    rows =  np.hstack((row_terminal,row,row,row))
+    columns = np.hstack((column_terminal,column_no,column_bad,column_good))
+    entry_terminal = np.ones(len(np.arange(len(Ns))[Ns==nmax]))
+    entry_no = 1-uq[Ns<nmax]
+    entry_bad = (uq*(1 - (a+Ns)/(a+b+Ns) ))[Ns<nmax]
+    entry_good = (uq*((a+Ns)/(a+b+Ns)))[Ns<nmax]
+    entries = np.hstack((entry_terminal,entry_no,entry_bad,entry_good))
+    return csr_matrix((entries, (rows,columns)), shape = (len(Ns), len(Ns)))
+
+def dT_s(p,P,s,dq,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params  
+    #uq =  (1-(1-q*upsilon_r)**mu).flatten()
+    duq = (dq*upsilon_r).flatten()
+    Ns = S[:,1]
+    row = np.arange(len(Ns))[Ns<nmax]
+    column_no = np.arange(len(Ns))[Ns<nmax]
+    column_bad = np.arange(len(Ns))[Ns<nmax]+Ns[Ns<nmax]+1
+    column_good = np.arange(len(Ns))[Ns<nmax]+Ns[Ns<nmax]+2
+    rows =  np.hstack((row,row,row))
+    columns = np.hstack((column_no,column_bad,column_good))
+    entry_no = -duq[Ns<nmax]
+    entry_bad = (duq*(1 - (a+Ns)/(a+b+Ns) ))[Ns<nmax]
+    entry_good = (duq*((a+Ns)/(a+b+Ns)))[Ns<nmax]
+    entries = np.hstack((entry_no,entry_bad,entry_good))
+    return csr_matrix((entries, (rows,columns)), shape = (len(Ns), len(Ns)))
+
+def d2T_s(p,P,s,d2q,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params  
+    d2uq = (d2q*upsilon_r).flatten()
+    Ns = S[:,1]
+    row = np.arange(len(Ns))[Ns<nmax]
+    column_no = np.arange(len(Ns))[Ns<nmax]
+    column_bad = np.arange(len(Ns))[Ns<nmax]+Ns[Ns<nmax]+1
+    column_good = np.arange(len(Ns))[Ns<nmax]+Ns[Ns<nmax]+2
+    rows =  np.hstack((row,row,row))
+    columns = np.hstack((column_no,column_bad,column_good))
+    entry_no = -d2uq[Ns<nmax]
+    entry_bad = (d2uq*(1 - (a+Ns)/(a+b+Ns) ))[Ns<nmax]
+    entry_good = (d2uq*((a+Ns)/(a+b+Ns)))[Ns<nmax]
+    entries = np.hstack((entry_no,entry_bad,entry_good))
+    return csr_matrix((entries, (rows,columns)), shape = (len(Ns), len(Ns)))
+
+def F_s(p,P,s,q,chi,lamb,theta,t,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    lamb1, lamb2, lamb3, lamb4 = lamb    
+    uq = (q*upsilon_r).flatten()
+    Ns = np.append(S[:,1],np.array([np.nan,np.nan,np.nan,np.nan]))
+    row_terminal = np.arange(len(Ns))[Ns==nmax]
+    row = np.arange(len(Ns))[Ns<nmax]
+    row_entry1 = np.repeat(np.arange(len(Ns))[np.isnan(Ns)][0],8)
+    row_entry2 = np.repeat(np.arange(len(Ns))[np.isnan(Ns)][1],8)
+    row_entry3 = np.repeat(np.arange(len(Ns))[np.isnan(Ns)][2],8)
+    row_entry4 = np.repeat(np.arange(len(Ns))[np.isnan(Ns)][3],8)
+    row_exit = np.arange(len(Ns))[:-4]
+    column_terminal = np.arange(len(Ns))[Ns==nmax]
+    column_no = np.arange(len(Ns))[Ns<nmax]
+    column_bad = np.arange(len(Ns))[Ns<nmax]+Ns[Ns<nmax]+1
+    column_good = np.arange(len(Ns))[Ns<nmax]+Ns[Ns<nmax]+2
+    column_entry = np.arange(len(Ns))[(Ns==0) | (np.isnan(Ns))]
+    column_exit1 = np.repeat(np.arange(len(Ns))[np.isnan(Ns)][0],len(Ns)-4)  
+    column_exit2 = np.repeat(np.arange(len(Ns))[np.isnan(Ns)][1],len(Ns)-4)
+    column_exit3 = np.repeat(np.arange(len(Ns))[np.isnan(Ns)][2],len(Ns)-4)
+    column_exit4 = np.repeat(np.arange(len(Ns))[np.isnan(Ns)][3],len(Ns)-4)
+    rows = np.hstack((row_terminal,row,row,row,row_entry1,row_entry2,row_entry3,row_entry4,row_exit,row_exit,row_exit,row_exit))
+    columns = np.hstack((column_terminal,column_no,column_bad,column_good,column_entry,column_entry,column_entry,column_entry,column_exit1,column_exit2,column_exit3,column_exit4))
+    entry_terminal = ((1-chi)*np.ones(len(Ns)-4))[Ns[:-4]==nmax]
+    entry_no = ((1-chi)*(1-uq))[Ns[:-4]<nmax]
+    entry_bad = ((1-chi)*uq*(1 - (a+Ns[:-4])/(a+b+Ns[:-4]) ))[Ns[:-4]<nmax]
+    entry_good = ((1-chi)*uq*((a+Ns[:-4])/(a+b+Ns[:-4])))[Ns[:-4]<nmax]   
+    entry_entry1 = [lamb1,0,0,0,1-lamb1,0,0,0]
+    entry_entry2 = [0,lamb2,0,0,0,1-lamb2,0,0]
+    entry_entry3 = [0,0,lamb3,0,0,0,1-lamb3,0]
+    entry_entry4 = [0,0,0,lamb4,0,0,0,1-lamb4]
+    entry_exit = chi/4
+    entries = np.hstack((entry_terminal,entry_no,entry_bad,entry_good,entry_entry1,entry_entry2,entry_entry3,entry_entry4,entry_exit,entry_exit,entry_exit,entry_exit))
+    return csr_matrix((entries, (rows,columns)), shape = (len(Ns), len(Ns)))
+
+def solver(theta,c,guess,t,Sub,tol,params):
+    a,b,alpha,beta,gamma,sigma = theta
+    kappa1, kappa2, kappa3, kappa4 = c[:4]
+    phi1, phi2, phi3, phi4 = c[4:]
+    phi_bar = np.array([np.repeat([phi1,phi2,phi3,phi4],231)]).T
+    P_init,s_init,V_init = guess
+    delta,f,J,mu,nmax,S,upsilon_r = params
+    V_old = V_init
+    P_old = P_init
+    s_old = s_init
+    dV = 1e10
+    change = 1e10
+    diff = 1e10
+    dP = 1e10
+    while diff>tol:
+        if (change > 0.01):
+            P0 = P_init
+            P1 = P0 - dV_s(P0,P_old,s_old,V_old,theta,phi_bar,t,params)/d2V_s(P0,P_old,s_old,V_old,theta,phi_bar,t,params)
+            P1 = np.where(np.isnan(P1) == True,P_old,np.where((P1<0),0,np.where((P1>1000),1000,P1)))
+                
+            dP = np.max(np.abs(P1 - P0))
+            print('dP:',dP)
+            #P0 = P1
+            P_new = P1
+            dVprim = dV
+        else:
+            P_new = P_old
+        q_new = q_s(P_new,P_new,s_old,theta,t,params)
+        T = T_s(P_new,P_new,s_old,q_new,theta,t,params)
+        eV = T @ V_old
+        V_new = 30*(q_new*P_new.T) + Sub + delta*eV - (phi_bar - np.exp(-delta*eV/phi_bar)*phi_bar)
+        eV = T @ V_new
+        chi = np.exp(-delta*eV/phi_bar).flatten()
+        lamb = (1-np.exp(-delta*V_new.reshape((231,4),order='F')[0,:]/[kappa1,kappa2,kappa3,kappa4]))
+        F = F_s(P_new,P_new,s_old,q_new,chi,lamb,theta,t,params)
+        s_new = (np.array([np.append(s_old,np.array([J/4-s_old[0,:231].sum(),J/4-s_old[0,231:462].sum(),J/4-s_old[0,462:693].sum(),J/4-s_old[0,693:].sum()]))])@ F)[:1,:-4]
+        while np.max(np.abs(s_new - s_old))>10e-3:
+            s_old = s_new
+            s_new = (np.array([np.append(s_old,np.array([J/4-s_old[0,:231].sum(),J/4-s_old[0,231:462].sum(),J/4-s_old[0,462:693].sum(),J/4-s_old[0,693:].sum()]))])@ F)[:1,:-4]   
+        dV = np.max(np.abs(V_new - V_old))
+        dP = np.max(np.abs(P_new - P_old))
+        ds = np.max(np.abs(s_new - s_old))
+        print('dV:',dV)
+        print('dP:',dP)
+        print('ds:',ds)
+        diff = max(dV,dP,ds)
+        change = (dVprim - dV)/dVprim
+        V_old = V_new
+        P_old = P_new
+        s_old = s_new
+    return [V_new,s_new,P_new,chi,lamb]
--- code/main.py
+++ code/main.py
@@ -6,53 +6,136 @@ Created on Thu Mar 16 09:20:40 2023
 """
 
 import numpy as np
+import pandas as pd
+import os
+from scipy.optimize import minimize
+from scipy.special import expit
 
-################################  Parameters  #################################
+os.chdir(os.path.dirname(os.path.abspath('__file__')))
 
-alpha = -.0105
-beta = np.array([-13.9974,-13.9974+0.1884,-13.9974+0.3084,-13.9974+0.4544])
-gamma = 3.4554
-a = 7.8516
-b = 4.3545
-kappa_bar = 25106
-phi_bar = 18535
-delta = .99
-f = .142
-mu = 20000
-upsilon_a = 0.019
-nmax = 20
-J = 2000
+# Define parameters
+a = 12.2260
+b = 2.1134
+alpha = -0.0068
+beta = np.array([-12.5906,-12.1095,-11.7011,-11.3012])
+gamma = 4.8860
 
 theta = [a,b,alpha,beta,gamma]
-c = [kappa_bar,phi_bar]
 
-################################  Type space  #################################
+kappa_bar = [55496,96673,161946,270623]
+phi_bar = [2580,3577,4562,5751]
+
+c = np.append(kappa_bar,phi_bar)
+
+# Define type space
 
 from functions import cartesian_product
 
+delta = .995
+tol = 1e-6
+f = .142
+upsilon_r = 0.7041
+nmax = 20
+mu = 10000
+J = 10000
+
+    # ...
 S = cartesian_product(np.arange(nmax+1),np.arange(nmax+1))
+
+    # ...
 S = S[S[:,0]<=S[:,1]]
+
+    # ...
 S=S[np.lexsort((S[:,0],S[:,1]))]
-S=np.repeat(S[:,:,np.newaxis],4,axis=2)
 
-upsilon_r = 0.2938*np.log(S[:,:1,0]+1)
+    # ...
+S=np.vstack((np.hstack((S,np.repeat(np.array([[1,0,0,0]]),len(S),axis=0))),
+             np.hstack((S,np.repeat(np.array([[0,1,0,0]]),len(S),axis=0))),
+             np.hstack((S,np.repeat(np.array([[0,0,1,0]]),len(S),axis=0))),
+             np.hstack((S,np.repeat(np.array([[0,0,0,1]]),len(S),axis=0)))
+             ))
 
-###############################  Set of prices  ###############################
+params = [delta,f,J,mu,nmax,S,upsilon_r]
 
-prices = np.array([np.linspace(0,300,301)])
+# Load functions
 
-##############################  Load functions  ###############################
+    # Load all functions
+from functions import q_s,solver
 
-    # Load utility function
-from functions import U
+P_init = np.array([[300]*len(S)])
+s_init = np.array([[J/(2*len(S))]*len(S)])
+V_init = (30*q_s(300,P_init,s_init,theta,0,params)*P_init.T)/(1-delta)
 
-    # Load demand function
-from functions import q
+V_star,s_star,P_star,chi_star,lamb_star = solver(theta,c,[P_init,s_init,V_init],tol,params)
+s_star = np.where(s_star<0,0,s_star)
 
-    # Load profit function
-from functions import pi
-pi(P_init,s_init,theta,0,S,f,mu,prices)
+# Generating 4 years of data
+for t in range(1,13*4+1):
+    if t == 1:
+        index = np.repeat(range(0,924),np.random.multinomial(s_star.sum().astype(int),(s_star/s_star.sum()).flatten(),size=(13*4,))[t-1,:])
+        p = (P_star.T + np.random.normal(loc = 0, scale = 25.0, size = (P_star.T).shape))
+        data = np.hstack(( np.zeros((len(index),1)) + t, np.array([index]).T, S[index,:], p[index,:], q_s(p,p,s_star,theta,0,params)[index,:] + np.random.normal(loc = 0, scale = 0.1, size = q_s(p,P_star,s_star,theta,0,params)[index,:].shape ) ))
+    else:
+        p = (P_star.T + np.random.normal(loc = 0, scale = 25.0, size = (P_star.T).shape))
+        data = np.vstack((data, np.hstack(( np.zeros((len(index),1)) + t, np.array([index]).T, S[index,:], p[index,:], q_s(p,p,s_star,theta,0,params)[index,:] + np.random.normal(loc = 0, scale = 0.1, size = q_s(P_star,P_star,s_star,theta,0,params)[index,:].shape ) )) ))
 
-np.diagonal(U(1,theta,0,S,f),axis1=0,axis2=1).reshape((len(S)*4,1))
+data = np.where(data<0,0,data)
 
-U(P_init,theta,0,S,f)
+data = pd.DataFrame(data,columns=['period','x','K','N','type 1','type 2','type 3','type 4','p','q'])
+
+data.to_pickle('data/data.pkl')
+
+
+# Inversion
+
+    # Compute market shares
+data['share'] = -np.log(1 - data['q'])/mu
+data = data[data['share'] > 0]
+data['share_0'] = 1 - data.groupby(['period'])['share'].transform('sum')
+data['r'] = 1 + 4*data['K']/data['N']
+data = data[(data['share'] > 0) & (data['r'] >0)]
+
+# GMM
+
+start_values = [0,0,0,-10,-10,-10,-10,0]
+
+from functions import dO,dU,O,xi,Z
+
+    # 1st stage
+W1 = np.linalg.inv( ((Z(start_values,data,params)).T @ (Z(start_values,data,params)))/len(data))
+#W1 = np.identity(8)
+res_demand = minimize(O, start_values, args=(data,W1,params), method='BFGS',jac=dO)
+xi_hat = xi(res_demand.x,data,params)
+
+    # 2nd stage
+W2 = np.linalg.inv( ((xi_hat*Z(res_demand.x,data,params)).T @ (xi_hat*Z(res_demand.x,data,params)))/len(data) )
+res_demand = minimize(O, start_values, args=(data,W2,params), method='BFGS',jac=dO)
+
+theta_hat = [expit(res_demand.x[0])*np.exp(res_demand.x[1]),
+(1-expit(res_demand.x[0]))*np.exp(res_demand.x[1]),
+res_demand.x[2],
+res_demand.x[3:7],
+res_demand.x[7]]
+
+    # Standard errors
+G_bar = ( (Z(res_demand.x,data,params).T @ (-dU(res_demand.x,data,params))) )/len(data)
+W2 = np.linalg.inv( ((xi_hat*Z(res_demand.x,data,params)).T @ (xi_hat*Z(res_demand.x,data,params)))/len(data) )
+S_hat = np.diag(np.linalg.inv((G_bar.T @ W2) @ G_bar))**.5/len(data)
+print('Heteroskedasticity:',S_hat)
+
+# Supply estimation
+
+from functions import l
+
+    # Compute empirical state distribution
+s_d = np.array([(data.groupby(['x'])['period'].count()/data.groupby(['period']).mean().shape[0]).reindex(np.arange(0,len(S)), fill_value=0)])
+
+    # Set tolerance level
+tol = 1e-3
+
+k0 = np.log([50000,100000,150000,200000,2500,3500,4500,5500])
+ 
+    # Maximize log-likelihood.
+res_supply = minimize(l, k0, args=(theta_hat,[P_init,s_init,V_init],tol,s_d,params), method='BFGS')
+
+    # Standard errors.
